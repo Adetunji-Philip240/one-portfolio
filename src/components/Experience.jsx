317cae82f1ff1d7899a81f803b91e33ed3162670
Adetunji-Philip240 <adewalegithub240@gmail.com>
2025-05-03 23:04:20 +0100

    Updating the portfolio

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import html from "../mypic/html.png";
 import css from "../mypic/css.png";
 import javascript from "../mypic/javascript.png";
@@ -8,8 +8,11 @@ import java from "../mypic/java.webp";
 import python from "../mypic/python.webp";
 import c from "../mypic/c.png";
 import kotlin from "../mypic/kotlin.png";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import nextjs from "../mypic/nextjs-react.png";
+import php from "../mypic/php.png";
+import mysql from "../mypic/mysql.png";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Experience = () => {
   useEffect(() => {
@@ -20,64 +23,71 @@ const Experience = () => {
       id: 1,
       src: html,
       title: "HTML",
-      style: "shadow-orange-500",
     },
     {
       id: 2,
       src: css,
       title: "CSS",
-      style: "shadow-blue-500",
     },
     {
       id: 3,
       src: javascript,
       title: "JAVASCRIPT",
-      style: "shadow-yellow-500",
     },
     {
       id: 4,
       src: react,
       title: "React",
-      style: "shadow-blue-600",
     },
     {
       id: 5,
       src: tailwind,
       title: "Tailwind",
-      style: "shadow-sky-400",
     },
     {
       id: 6,
       src: java,
       title: "JAVA",
-      style: "shadow-blue-600",
     },
     {
       id: 7,
       src: python,
       title: "Python",
-      style: "shadow-blue-400",
     },
     {
       id: 8,
       src: c,
       title: "C",
-      style: "shadow-blue-500",
     },
     {
       id: 9,
       src: kotlin,
       title: "Kotlin",
-      style: "shadow-blue-500",
+    },
+    {
+      id: 10,
+      src: nextjs,
+      title: "Next.js",
+    },
+    {
+      id: 11,
+      src: php,
+      title: "PHP",
+    },
+    {
+      id: 12,
+      src: mysql,
+      title: "MySQL",
     },
   ];
 
   return (
-    <div
-      name="experience"
-      className="w-full h-screen"
-    >
-      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white" data-aos="fade-up" data-aos-duration="2000">
+    <div name="experience" className="w-full h-screen ">
+      <div
+        className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white"
+        data-aos="fade-up"
+        data-aos-duration="2000"
+      >
         <div id="experience">
           <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
             Experience
@@ -89,7 +99,7 @@ const Experience = () => {
           {techs.map(({ id, src, title, style }) => (
             <div
               key={id}
-              className="shadow-md hover:scale-105 duration-500 py-2 rounded-lg "
+              className="shadow-md shadow-blue-300 hover:scale-105 duration-500 py-2 rounded-lg"
             >
               <img src={src} alt="" className="w-20 mx-auto" />
               <p className="mt-4">{title}</p>
